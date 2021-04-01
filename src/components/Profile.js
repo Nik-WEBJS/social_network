e909fcc43a4312a665fd54622afb403dfaf85f5f
Nik <nikita.fokin0123@gmail.com>
2021-04-01 19:55:52 +0600

    create header,navbar,profile

---
+++ src/components/Profile.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='Profile'>
+            <div>
+                <img className='img-fon' src='https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg' />
+            </div>
+            <div>
+                ava + desription
+            </div>
+            <div>
+                my post
+            </div>
+            <div>
+                new post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    )
+}
+
+export default Profile
